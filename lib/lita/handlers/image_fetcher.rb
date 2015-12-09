25cacdfa025f45fbd55af3f3d376be0b61a72690
Chris Korhonen <ckorhonen@gmail.com>
2015-12-09 14:27:35 -0500

    Update image_fetcher.rb

--- lib/lita/handlers/image_fetcher.rb
+++ lib/lita/handlers/image_fetcher.rb
@@ -2,10 +2,6 @@ module Lita
   module Handlers
     class ImageFetcher < Handler
       config :pixabay_key, types: [String]
-      config :google_cse_key, types: [String]
-      config :google_cse_id, types: [String]
-      config :google_cse_safe_search, types: [Symbol], default: :high
-      config :bing_key, types: [String]
 
       route(/(?:image|img)(?:\s+me)? (.+)/i, :fetch, command: true, help: {
         "image QUERY" => "Displays an image matching the query."
@@ -13,13 +9,7 @@ module Lita
 
       def sources
         @sources ||= {}
-        @sources[:pixabay] ||= Handlers::Pixabay.new(api_key: config.pixabay_key) if config.pixabay_key.present?
-        @sources[:google_cse] ||= Handlers::GoogleCSE.new(
-            cse_id: config.google_cse_id,
-            cse_key: config.google_cse_key,
-            cse_safe_search: config.google_cse_safe_search
-          ) if config.google_cse_id.present? && config.google_cse_safe_search.present? && config.google_cse_key.present?
-        @sources[:bing] ||= Handlers::Bing.new(bing_key: config.bing_key) if config.bing_key.present?
+        @sources[:pixabay] ||= Handlers::Pixabay.new(api_key: config.pixabay_key)
         @sources
       end
 
